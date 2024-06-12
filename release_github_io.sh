1b3c2ae4b7293583d05180d23ef68ab5ebb0c65b
杨云飞 <2313802205@gmail.com>
2024-06-12 19:01:38 +0800

    Commit current changes before pull

--- release_github_io.sh
+++ release_github_io.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # 将文件同步到 GitHub Pages 仓库中
-# 请将以下路径替换为您的 GitHub Pages 仓库路径
 
 REPO_PATH="/home/yyf/workspace/xcy995-github-io"
 
 # 使用 rsync 命令将文件同步到 GitHub Pages 仓库中
-# 您可以根据需要修改路径和同步的文件
 
 rsync -avcz --progress --delete ./css ./fonts ./images ./js "$REPO_PATH/seia/"
 rsync -avcz --progress 系统工程师文档.html "$REPO_PATH/seia/index.html"
